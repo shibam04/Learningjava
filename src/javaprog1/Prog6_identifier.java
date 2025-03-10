03cbd26479de274c3db77608df099a4e9b377d53
shibam <shibam.seal04@gmail.com>
2025-03-10 23:28:18 +0530

    Identifier Rule

---
+++ src/javaprog1/Prog6_identifier.java
@@ -0,0 +1,13 @@
+package javaprog1;
+
+public class Prog6_identifier
+{
+    public static void main(String[] args) {
+        int _a=10;
+        int $x=20;
+        int s=30;
+        System.out.println("Print:"+_a);
+        System.out.println("Print:"+$x);
+        System.out.println("Print:"+s);
+    }
+}
