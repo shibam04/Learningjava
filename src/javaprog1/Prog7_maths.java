1197df08b065c3b91b63382e750a742cf80c7da1
shibam <shibam.seal04@gmail.com>
2025-03-10 23:37:11 +0530

    Maths Rule

---
+++ src/javaprog1/Prog7_maths.java
@@ -0,0 +1,12 @@
+package javaprog1;
+
+public class Prog7_maths
+{
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+        System.out.println(0/2);
+    }
+}
