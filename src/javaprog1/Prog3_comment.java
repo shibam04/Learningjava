73ae565d82bc5bf44c0b4d94aa23cd61d98ec3d8
shibam <shibam.seal04@gmail.com>
2025-03-10 22:53:22 +0530

    Single Line Comment

---
+++ src/javaprog1/Prog3_comment.java
@@ -0,0 +1,9 @@
+package javaprog1;
+
+public class Prog3_comment
+{
+    public static void main(String[] args) {
+        //Single Line Comment
+        System.out.println("Single Comment");
+    }
+}
