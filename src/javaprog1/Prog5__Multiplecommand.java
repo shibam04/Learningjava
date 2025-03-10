d8c8b1bd6717ae15c10899b84bea1c91e0655d4a
shibam <shibam.seal04@gmail.com>
2025-03-10 23:27:39 +0530

    Multiple Command

---
+++ src/javaprog1/Prog5__Multiplecommand.java
@@ -0,0 +1,10 @@
+package javaprog1;
+
+public class Prog5__Multiplecommand
+{
+    public static void main(String[] args) {
+        System.out.println("Multiple Line");
+        System.out.println("Multiple Line");
+        System.out.println("Multiple Line");
+    }
+}
