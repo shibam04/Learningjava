0203de3b4d2c5ea408aef73e4d547de480575ad1
shibam <shibam.seal04@gmail.com>
2025-03-10 23:48:18 +0530

    SytemOutPrint vs SytemOutPrintln

---
+++ src/javaprog1/Prog8_printline.java
@@ -0,0 +1,10 @@
+package javaprog1;
+
+public class Prog8_printline
+{
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.println("World");
+        System.out.println("Prog");
+    }
+}
