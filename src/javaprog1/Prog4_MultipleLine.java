b266230b8de6027867bf7295cbf0930e468d47d1
shibam <shibam.seal04@gmail.com>
2025-03-10 23:06:24 +0530

    Multiple Line Comment

---
+++ src/javaprog1/Prog4_MultipleLine.java
@@ -0,0 +1,12 @@
+package javaprog1;
+
+public class Prog4_MultipleLine
+{
+    public static void main(String[] args) {
+        /**
+         * Author:Shibam
+         * Learning: Java
+         **/
+        System.out.println("Multiple Line Comment");
+    }
+}
