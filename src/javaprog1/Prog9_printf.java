85873dfce95ca2327a6d3fa312895fca475e2762
shibam <shibam.seal04@gmail.com>
2025-03-11 23:30:00 +0530

    Nine Table using Printf

---
+++ src/javaprog1/Prog9_printf.java
@@ -0,0 +1,20 @@
+package javaprog1;
+
+public class Prog9_printf
+{
+    public static void main(String[] args) {
+        int nine = 9;
+        System.out.printf("Table of %d\n",nine);
+        System.out.printf("Table of %d * 1: %d\n", nine,nine);
+        System.out.printf("Table of %d * 2: %d\n", nine,nine * 2);
+        System.out.printf("Table of %d * 3: %d\n", nine,nine * 3);
+        System.out.printf("Table of %d * 4: %d\n", nine,nine * 4);
+        System.out.printf("Table of %d * 5: %d\n", nine,nine * 5);
+        System.out.printf("Table of %d * 6: %d\n", nine,nine * 6);
+        System.out.printf("Table of %d * 7: %d\n", nine,nine * 7);
+        System.out.printf("Table of %d * 8: %d\n", nine,nine * 8);
+        System.out.printf("Table of %d * 9: %d\n", nine,nine * 9);
+        System.out.printf("Table of %d * 10: %d\n", nine,nine * 10);
+
+    }
+}
