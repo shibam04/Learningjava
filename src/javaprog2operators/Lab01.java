ad3289838c712ff181c00075f77e7190a1c403a1
shibam <shibam.seal04@gmail.com>
2025-03-13 01:32:01 +0530

    Math Operations

--- src/javaprog2operators/Lab01.java
+++ src/javaprog2operators/Lab01.java
@@ -9,10 +9,15 @@ public class Lab01
         int z=10;
         double a;
         double b;
+        double c;
         a=Math.pow(x,2)+Math.pow(y,2)-z;
+        //Way 1
         b=Math.pow(a,1.0/3.0);
+        //Way 2
+        c=Math.cbrt(a);
         System.out.println(a);
         System.out.println(b);
+        System.out.println(c);
 
 
     }
