cacc2e4dfe9ca7c8c588d7c8fe8203fb947354bb
shibam <shibam.seal04@gmail.com>
2025-03-23 23:52:41 +0530

    For Loop

---
+++ src/ex10_forloop/Prg3_withoutcond.java
@@ -0,0 +1,12 @@
+package ex10_forloop;
+
+public class Prg3_withoutcond
+{
+    public static void main(String[] args)
+    {
+         for(int _1=0;;_1++)
+         {
+             System.out.println(_1);
+         }
+    }
+}
